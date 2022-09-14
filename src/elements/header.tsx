636527b6debffc7662ceccdc795dae882c21d5d1
Bartek Kruszczynski <kruszczyk@gmail.com>
2022-09-14 14:48:20 +0200

    Fix header spacing

--- src/elements/header.tsx
+++ src/elements/header.tsx
@@ -8,6 +8,7 @@ export function Header({ children }) {
 const H2 = styled.h2`
   font-size: 60px;
   line-height: 80px;
-  margin-bottom: 60px;
+  margin: 0 auto 60px;
+  max-width: 1000px;
   padding: 0 100px;
 `;
