fdd33843b97577aecf5bc349891734cea6d093cb
Bartek Kruszczynski <kruszczyk@gmail.com>
2023-01-08 15:56:41 +0100

    Override the path warning

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,9 @@ require("esbuild").build({
   entryPoints: ["src/app.tsx"],
   bundle: true,
   minify: true,
+  logOverride: {
+    "different-path-case": "info",
+  },
   plugins: [],
   outfile: "public/out.js",
 }).catch(() => process.exit(1));
