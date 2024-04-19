6284b200b0df52874038244d3fe62073a77610f8
Bartek Kruszczynski <kruszczyk@gmail.com>
2024-04-19 22:03:05 +0200

    Project grid on css modules

---
+++ src/components/ProjectsGrid/ProjectItem.tsx
@@ -0,0 +1,30 @@
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import { useAsset } from "../../hooks/contentful";
+import styles from "./ProjectItem.module.css";
+
+export function ProjectItem({ project }: any) {
+  const { data, loading } = useAsset(project.fields.mainImage.sys.id);
+  const { fields } = data;
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <li className={styles.item} key={project.sys.id}>
+      <Link
+        style={
+          {
+            "--sq-bg-image": `url("${fields.file.url}")`,
+            "--sq-bg-color": project.fields.color,
+          } as CSSProperties
+        }
+        className={styles.squareLink}
+        to={`${project.sys.id}`}
+      >
+        <span>{project.fields.projectName}</span>
+      </Link>
+    </li>
+  );
+}
---
+++ src/components/ProjectsGrid/ProjectsGrid.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import styles from "./ProjectsGrid.module.css";
+
+import { ProjectType } from "../../types";
+import { useGlobalBackground } from "../../state/global";
+import { ProjectItem } from "./ProjectItem";
+import { Filter } from "./Filter";
+
+export function ProjectsGrid({ projects }: any) {
+  const [filter, setFilter] = useState<ProjectType>("All");
+  const filteredProjects = filter !== "All"
+    ? projects.filter((project: any) => project.fields.projectType.includes(filter))
+    : projects;
+
+  const [, setBackground] = useGlobalBackground();
+
+  useEffect(() => {
+    setBackground({ type: "Full", color: "#FFFFFF" });
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ul className={styles.grid}>
+        {filteredProjects.map((item: any) => (
+          <ProjectItem key={`pg-item-${item.sys.id}`} project={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
