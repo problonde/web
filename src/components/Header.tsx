2ce4c8d5707df417d5e4d6bd049afb398ce31d2d
Bartek Kruszczynski <kruszczyk@gmail.com>
2024-04-19 21:10:38 +0200

    Move `<Header>` to CSS modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "./Header.module.css";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export function Header({ children }: Props) {
-  return <H2>{children}</H2>;
+  return <h2 className={styles.header}>{children}</h2>;
 }
-
-const H2 = styled.h2`
-  font-size: 60px;
-  line-height: 80px;
-  margin: 0 auto 60px;
-  max-width: 1000px;
-  padding: 0 100px;
-`;
