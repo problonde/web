1567bb09091000a0adc561845fb8e74f4972ab68
Bartek Kruszczynski <kruszczyk@gmail.com>
2024-04-19 21:13:15 +0200

    Move `<Paragraph>` to CSS modules

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,5 +1,6 @@
+import classNames from "classnames";
 import React, { Fragment } from "react";
-import styled from "styled-components";
+import styles from "./Paragraph.module.css";
 
 type Text = {
   nodeType: "text";
@@ -27,9 +28,13 @@ function contentToTag(content: Content) {
       return <Fragment key={content.value}>{content.value}</Fragment>;
     case "hyperlink":
       return (
-        <Link key={content.data.uri} href={content.data.uri}>
+        <a
+          className={styles.link}
+          key={content.data.uri}
+          href={content.data.uri}
+        >
           {content.content[0].value}
-        </Link>
+        </a>
       );
     default:
       return null;
@@ -38,27 +43,8 @@ function contentToTag(content: Content) {
 
 export function Paragraph({ className, content }: Props) {
   return (
-    <P className={className}>
+    <p className={classNames(styles.paragraph, className)}>
       {content.map((inner: Content) => contentToTag(inner))}
-    </P>
+    </p>
   );
 }
-
-const P = styled.p`
-  font-size: 1.8rem;
-  line-height: 2.25rem;
-  margin: 0 auto;
-  max-width: 63rem;
-  padding: 0 6rem 4rem;
-  white-space: pre-line;
-`;
-
-const Link = styled.a`
-  color: #000000;
-  text-decoration-color: rgba(0, 0, 0, 0);
-  transition: all ease-in-out 0.2s;
-
-  &:hover {
-    text-decoration-color: rgba(0, 0, 0, 1);
-  }
-`;
