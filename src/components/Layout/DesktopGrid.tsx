e4a1b06237627422d4e089dd77e193851d2227ef
Bartek Kruszczynski <kruszczyk@gmail.com>
2024-07-08 11:09:06 +0200

    Starting on grid menu

---
+++ src/components/Layout/DesktopGrid.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./DesktopGrid.module.css";
+export function DesktopGrid() {
+  return (
+    <div className={styles.grid}>
+      <div>A</div>
+      <div>B</div>
+      <div>C</div>
+      <div>D</div>
+      <div>E</div>
+      <div>F</div>
+      <div>G</div>
+      <div>H</div>
+      <div>I</div>
+    </div>
+  );
+}
