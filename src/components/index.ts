ebf22e46106bfbce9e743f35fa5038dea81114f9
Bartek Kruszczynski <kruszczyk@gmail.com>
2024-11-11 10:40:29 +0100

    Explicit exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
 export { Asset } from "./Asset";
 export { Body } from "./Body";
-export * from "./ContactWrapper";
+export { ContactWrapper } from "./ContactWrapper";
 export { ContentNode } from "./ContentNode";
 export { Header } from "./Header";
-export * from "./Layout";
+export { Layout } from "./Layout";
 export { Paragraph } from "./Paragraph";
 export { ProjectDetail } from "./ProjectDetail";
-export * from "./ProjectsGrid";
+export { ProjectsGrid } from "./ProjectsGrid";
 export { ProjectsList } from "./ProjectsList";
 export { RootRedirect } from "./RootRedirect";
 export { WrappedAsset } from "./WrappedAsset";
